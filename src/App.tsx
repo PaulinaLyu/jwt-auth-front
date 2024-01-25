3cfb424c0abdd255aaf34a4dfe1d26427f1a57eb
Polina Lyubeznova <polina.lyubeznova@geogracom.com>
2024-01-25 15:22:15 +0300

    add(store/authStore): Добавила стор

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import { useEffect } from "react";
 import LoginForm from "./components/LoginForm/LoginForm";
+import { useAuthStore } from "./store/authStore";
 
 function App() {
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const logout = useAuthStore((state) => state.logout);
+  const isActivate = useAuthStore((state) => state.isActivate);
+  const isLoading = useAuthStore((state) => state.isLoading);
+  const user = useAuthStore((state) => state.user);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      checkAuth();
+    }
+  }, []);
+
+  if (!isLoading) {
+    return <div>Загрузка...</div>;
+  }
+
+  if (!isActivate) {
+    return <LoginForm />;
+  }
+
   return (
     <>
-      <LoginForm />
+      <h3>
+        {isActivate ? `Вы зарегистрированы. E-mail: ${user?.email}` : "Пожалуйста, авторизуйтесь"}
+      </h3>
+      <button onClick={() => logout()}>Выйти</button>
     </>
   );
 }
